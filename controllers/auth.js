73c84a9f43c63d48268aaa93c491b41e693827b9
Stanislav Polyanikov <53227792+Se-ki-zo@users.noreply.github.com>
2020-12-11 23:22:40 +0300

    cors 14

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,8 +46,8 @@ module.exports.login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: true,
+          httpOnly: false,
+          sameSite: false,
         }).send({
           token,
         })
