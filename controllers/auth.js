7c0ea12c00aaa59be7561ec5bb5d030fbc238525
Stanislav Polyanikov <53227792+Se-ki-zo@users.noreply.github.com>
2020-12-12 13:33:16 +0300

    cors 18

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,6 +48,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: false,
           sameSite: 'none',
+          secure: true,
         }).send({
           token,
         })
